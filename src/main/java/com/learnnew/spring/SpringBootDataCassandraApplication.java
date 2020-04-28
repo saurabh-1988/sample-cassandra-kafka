daebae3ec68530bc496c7aff27bd871828663758
Saurabh Sarma <saurabhsarma.ece@gmail.com>
2020-04-28 13:16:34 +0530

    initial commit

---
+++ src/main/java/com/learnnew/spring/SpringBootDataCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.learnnew.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataCassandraApplication.class, args);
+	}
+
+}
