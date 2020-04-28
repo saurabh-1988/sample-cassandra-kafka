d1cb2eb4d435a9e712d0f401edffabe630c1d97f
Saurabh Sarma <saurabhsarma.ece@gmail.com>
2020-04-28 18:40:31 +0530

    added missing reporitory annotation

--- src/main/java/com/learnnew/spring/data/cassandra/repository/TutorialRepository.java
+++ src/main/java/com/learnnew/spring/data/cassandra/repository/TutorialRepository.java
@@ -5,9 +5,10 @@ import java.util.UUID;
 
 import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import com.learnnew.spring.data.cassandra.model.Tutorial;
-
+@Repository
 public interface TutorialRepository extends CassandraRepository<Tutorial, UUID> {
   @AllowFiltering
   List<Tutorial> findByPublished(boolean published);
