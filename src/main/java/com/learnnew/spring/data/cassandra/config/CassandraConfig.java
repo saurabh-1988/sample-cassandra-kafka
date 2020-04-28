2cf969bab73b40fcd99811593389840bfcd997e3
Saurabh Sarma <saurabhsarma.ece@gmail.com>
2020-04-28 18:30:02 +0530

    config for cassandra for keyspace creation

---
+++ src/main/java/com/learnnew/spring/data/cassandra/config/CassandraConfig.java
@@ -0,0 +1,32 @@
+package com.learnnew.spring.data.cassandra.config;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories
+public class CassandraConfig extends AbstractCassandraConfiguration  {
+    @Value("${spring.data.cassandra.keyspacename}")
+    protected String keyspaceName;
+
+	@Override
+	protected String getKeyspaceName() {
+		return this.keyspaceName;
+	}
+	
+    @Override
+    protected List getKeyspaceCreations() {
+        return Collections.singletonList(CreateKeyspaceSpecification
+                .createKeyspace(keyspaceName).ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withSimpleReplication());
+    }
+    
+}
